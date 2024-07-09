b843b6981abbbbe57acf029b36001fabb19b606a
sneh-soni <ssneh20062003@gmail.com>
2024-07-10 00:12:51 +0530

    Initial commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://stackoverflow-f7nw.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
